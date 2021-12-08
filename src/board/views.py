bbc79ae9cfb07dfc66c5c5ea38a7078d5e4dfb06
Thomas <N10582835@qut.edu.au>
2021-12-08 13:58:23 +1000

    authentication check

--- src/board/views.py
+++ src/board/views.py
@@ -6,9 +6,20 @@ from .models import *
 # Create your views here.
 
 def board_view(request, board, *args, **kwargs): 
+
+    board = Board.objects.get(id=board)
+    boardmembers = BoardMember.objects.get(board=board)
+    lists = List.objects.get(board=board)
+    cards = Card.objects.get(board=board)
+
+    # if request.user in boardmembers
+    #     return render(request, "error.html")
+    # else:   
+
     context = {
-        "board": Board.objects.get(id=board),
-        "lists": List.objects.get(board=board),
-        "cards": Card.objects.get(board=board)
+        "board": board,
+        "members": boardmembers,
+        "lists": lists,
+        "cards": cards
     }
     return render(request, "b.html", context)
