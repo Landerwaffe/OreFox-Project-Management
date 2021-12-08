7e7a26f6f794c4404b7dc0991c8cd761e095f7b7
Thomas <N10582835@qut.edu.au>
2021-12-08 12:09:42 +1000

    board admin page

--- src/board/admin.py
+++ src/board/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Board)
+admin.site.register(BoardMember)
+admin.site.register(List)
+admin.site.register(Card)
+admin.site.register(CardContent)
+admin.site.register(BoardTag)
+admin.site.register(CardTag)
+admin.site.register(Reaction)
